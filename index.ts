98ddc9f136ca730fb8d68cbe3adb53eeed8ed31a
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-05-12 00:12:21 +0330

    Add /login route with unavailable feature message

--- index.ts
+++ index.ts
@@ -12,6 +12,11 @@ server.get("/mmd", (_, res) => {
   res.send("Sup mmd \n")
 })
 
+server.get("/login", (_, res) => {
+  res.setHeader("Content-Type", "text/plain")
+  res.send("This feature is unavailable")
+})
+
 const PORT = process.env["PORT"]
 
 server.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
